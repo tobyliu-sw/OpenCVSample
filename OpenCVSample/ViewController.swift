9990356a8bdacb01bff281e573fda98360d54e5b
Toby Liu <tobyliu-sw@users.noreply.github.com>
2017-06-26 13:06:05 -0700

    add taking a photo function by referring to the guide below. https://developer.apple.com/library/content/referencelibrary/GettingStarted/DevelopiOSAppsSwift/WorkWithViewControllers.html

--- OpenCVSample/ViewController.swift
+++ OpenCVSample/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func takePhoto(_ sender: UIBarButtonItem) {
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
 
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            // Only allow photos to be picked, not taken.
+            imagePickerController.sourceType = .camera
+
+            // Make sure ViewController is notified when the user picks an image.
+            imagePickerController.delegate = self
+            present(imagePickerController, animated: true, completion: nil)
+        }
+    }
+
+    @IBAction func grayscaleImage(_ sender: Any) {
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let takenImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+
+        // Set imageView to display the selected image.
+        imageView.image = takenImage
+
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
 }
 
